cde77c7c00f100d6887cd79423806a26f259f22d
brahim <brahim.akarouch@gmail.com>
2020-09-07 17:07:17 +0000

    Add search user functions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./compenents/layout/Navbar";
 import Users from "./compenents/users/Users";
+import Search from "./compenents/users/Search";
 import axios from "axios";
 
 class App extends Component {
@@ -16,10 +17,19 @@ class App extends Component {
     );
     this.setState({ loading: false, users: res.data });
   }
+
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ loading: false, users: res.data.items });
+  };
   render() {
     return (
       <div className="App">
         <Navbar title=" Github finder" icon="fab fa-github" />
+        <Search searchUsers={this.searchUsers} />
         <div className="container">
           <Users users={this.state.users} loading={this.state.loading} />
         </div>
---
+++ src/compenents/users/Search.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export class Search extends Component {
+  state = {
+    text: "",
+  };
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+  };
+
+  render() {
+    return (
+      <div>
+        <form className="form" onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            name="text"
+            placeholder="Search"
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input type="submit" className="btn btn-dark btn-block" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
