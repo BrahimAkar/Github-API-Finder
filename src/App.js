63679fb699ff8c5b1462737a62381862d4bf775d
brahim <brahim.akarouch@gmail.com>
2020-09-09 01:07:50 +0000

    Implement NotFound Route

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React, { Fragment, useState, useHook } from "react";
 import "./App.css";
 
 import Navbar from "./compenents/layout/Navbar";
-import Users from "./compenents/users/Users";
 import User from "./compenents/users/User";
-import Search from "./compenents/users/Search";
 import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import about from "./compenents/pages/about";
 import GithubState from "./context/github/GithubState";
-
+import Home from "./compenents/pages/Home";
+import NotFound from "./compenents/pages/NotFound";
 const App = () => {
   return (
     <GithubState>
@@ -18,18 +17,11 @@ const App = () => {
           <Navbar />
           <div className="container">
             <Switch>
-              <Route
-                exact
-                path="/"
-                render={(props) => (
-                  <Fragment>
-                    <Search />
-                    <Users />
-                  </Fragment>
-                )}
-              />
+              <Route exact path="/" component={Home} />
               <Route exact path="/about" component={about} />
               <Route exact path="/user/:login" component={User} />
+              <Route component={NotFound} />
+              
             </Switch>
           </div>
         </div>
